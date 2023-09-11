4dce6a77e1c49c7b61ad16816c982822220c2277
Flmbfle <erwantotet@gmail.com>
2023-09-11 11:32:00 +0200

    Exercice 6 JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,30 @@
-
+/*Entrez le Nom/Prénom*/
 var reponse1 = window.prompt("Saisissez votre Nom :");
 var reponse2 = window.prompt("Saisissez votre Prénom :");
 window.alert("Bonjour, " + reponse1 + " " + reponse2);
 
+/*Demande un Nombre a l'utilisateur puis calcule le produit*/
 var reponse3 = window.prompt("Saisissez un nombre :");
 var reponse4 = window.prompt("Saisissez un second nombre :");
 window.alert("Le produit des deux nombes est : " + reponse3 * reponse4);
 
+/*Demande la température en °C et convertie en °F*/
 var reponse5 = window.prompt("Quelle est la température en °C ?");
 F = (reponse5*9/5) + 32
 window.alert("La température est de : " + F + "°F");
 
+/*Affiche la chaine de caractères 100*/
 var a = "100"
 alert("Ceci est une chaine de caractères : " + a);
+
+/*Elément de décrémentation*/
 var b = 100
 var x = 101
 alert(b = --x);
+
 var c = 1.00
 alert(a + c);
+
 var d = true
 alert(d=false);
 
