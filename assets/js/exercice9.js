41f14483cb011aef7f7892d5903eca7c39accc27
Flmbfle <erwantotet@gmail.com>
2023-09-14 16:27:22 +0200

    Exercice 9 JS

--- assets/js/exercice9.js
+++ assets/js/exercice9.js
@@ -24,20 +24,35 @@ function afficheImg(image){
 afficheImg()
 
 //Exercice 2
-var str1= window.prompt("Saisissez une chaîne de mots");
-var str2= window.prompt("Saisissez une seconde chaîne de mots");
-var n= window.prompt('Saisissez un numéro')
-function strtok(str1 , str2 , n){
+
+function strtok(str1, str2, n) {
+    // Divisez la chaîne str1 en une liste de mots en utilisant str2 comme séparateur.
     var mots = str1.split(str2);
-    
+
+    // Vérifiez si le nième mot existe dans la liste.
     if (n - 1 >= 0 && n - 1 < mots.length) {
         // Retournez le nième mot.
-        return mots[n - 1];
+        return mots[n-1];
     } else {
         // Si le nième mot n'existe pas, retournez une chaîne vide.
         return "";
     }
 }
 
+// Demandez à l'utilisateur d'entrer les valeurs des paramètres.
+var str1 = prompt("Entrez la chaîne str1 :");
+var str2 = prompt("Entrez le caractère de séparation str2 :");
+var n = parseInt(prompt("Entrez la valeur de n :"));
+
+// Appelez la fonction strtok avec les valeurs entrées par l'utilisateur.
 var resultat = strtok(str1, str2, n);
-resultat = document.getElementById('text').innerHTML ="Le nième mot est : " + resultat
+
+// Récupérez l'élément HTML où vous voulez afficher le résultat.
+var textElement = document.getElementById("text");
+
+// Affichez le résultat.
+if (resultat !== "") {
+    textElement.innerHTML = "Le " + n + " ème mot est : " + resultat;
+} else {
+    textElement.innerHTML = "Le " + n + "ème mot(s) n'existe pas.";
+}
